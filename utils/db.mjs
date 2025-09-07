b9bfde3cad4c5b8e24d6026653bcaa64d52ab326
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2025-09-07 11:43:59 -0400

    fixed utils/db file

--- utils/db.mjs
+++ utils/db.mjs
@@ -1,6 +1,6 @@
 import pkg from 'mongodb';
-const { MongoClient } = pkg;
 
+const { MongoClient } = pkg;
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || 27017;
 const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
